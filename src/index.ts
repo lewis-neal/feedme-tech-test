292ee22b691bab13ac484dbe4bed6018d3c97afb
Lewis Neal <l_neal_@hotmail.co.uk>
2022-05-13 21:58:52 +0100

    Add todo list

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ const net = require('net');
 const conn = net.createConnection(8282, 'localhost');
 const { parse } = require('./MessageParser');
 const schema = require('./Schema');
-import { Message } from "./Messages";
-
-// todo - what happens when something goes wrong?
-// todo - closing connection
 
 schema.fetchSchema().then((typesSchema: any) => {
     conn.on('data', (data: Buffer) => {
