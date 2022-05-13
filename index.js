4820a884a227e8afe3adfa1cf043d242889889ca
Lewis Neal <l_neal_@hotmail.co.uk>
2022-05-13 16:08:21 +0100

    Add basic package.json

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const net = require('net');
 const conn = net.createConnection(8282, 'localhost');
 
+// todo - what happens when something goes wrong?
+// todo - closing connection
+
 conn.on('data', (data) => {
     console.log(data.toString());
 });
